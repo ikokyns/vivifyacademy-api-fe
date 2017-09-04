0390c92ecb01f84b82da9def07b61c38e88aec97
Goran Prijic <goran.prijic@gmail.com>
2017-09-04 17:50:07 +0200

    using listing action

--- src/app/shared/services/contacts.service.ts
+++ src/app/shared/services/contacts.service.ts
@@ -6,11 +6,7 @@ import { Observable, Observer } from 'rxjs';
 @Injectable()
 export class ContactsService {
 
-  private contacts: Contact[] = [
-    new Contact(1, 'John', 'Doe', 'john@example.com'),
-    new Contact(2, 'Daniel', 'Ros', 'daniel@example.com'),
-    new Contact(3, 'Martin', 'Hess', 'martin@example.com')
-  ];
+  private contacts: Contact[] = [];
   private idCount: number = 3;
 
   constructor(private http: HttpClient) { }
@@ -18,8 +14,17 @@ export class ContactsService {
   public getContacts()
   {
     return new Observable((o: Observer<any>) => {
-      o.next(this.contacts);
-      return o.complete();
+      this.http.get('http://localhost:8000/api/contacts')
+        .subscribe(
+          (contacts: any[]) => {
+            contacts.forEach(c => {
+              this.contacts.push(new Contact(c.id, c.first_name, c.last_name, c.email));
+            });
+
+            o.next(this.contacts);
+            return o.complete();
+          }
+        );
     });
   }
 
@@ -28,7 +33,7 @@ export class ContactsService {
     return new Observable((o: Observer<any>) => {
       this.idCount++;
       let c = new Contact(this.idCount, contact.firstName, contact.lastName, contact.email);
-      
+
       this.contacts.push(c);
       o.next(c);
       return o.complete();
