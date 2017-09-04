c332402533bafebb3a3862b7c79aed11bde2481c
Goran Prijic <goran.prijic@gmail.com>
2017-09-04 18:21:18 +0200

    Deleting resource

--- src/app/shared/services/contacts.service.ts
+++ src/app/shared/services/contacts.service.ts
@@ -73,11 +73,16 @@ export class ContactsService {
   public removeContact(contact: Contact)
   {
     return new Observable((o: Observer<any>) => {
-      const index = this.contacts.indexOf(contact);
-      this.contacts.splice(index, 1);
+      this.http.delete('http://localhost:8000/api/contacts/' + contact.id)
+        .subscribe(
+          () => {
+            const index = this.contacts.indexOf(contact);
+            this.contacts.splice(index, 1);
 
-      o.next(index);
-      return o.complete();
+            o.next(index);
+            return o.complete();
+          }
+        );
     });
   }
 
